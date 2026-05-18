d877a59450b6c3b751158555f76a0ee3a6df6e1b
Mahliyo <gasandigital13@gmail.com>
2026-05-18 06:25:25 +0500

    Admin Panel struktura

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom";
 import "./Sidebar.css";
 import React from 'react'
+import { FaPhone } from "react-icons/fa6";
+import { GiTeacher } from "react-icons/gi";
 function Sidebar() {
   return (
     <div className="container">
         <h1 className="sidebar-title">Logo</h1>
         <ul className="sidebar-list">
-            <li><Link className="sidebat-link" to="contacts"></Link></li>
-            <li><Link className="sidebat-link" to="teachers"></Link></li>
-            <li><Link className="sidebat-link" to="students"></Link></li>
+            <li><Link className="sidebat-link" to="contacts">Contacts <FaPhone /> </Link></li>
+            <li><Link className="sidebat-link" to="teachers">Teachers <GiTeacher /> </Link></li>
+            <li><Link className="sidebat-link" to="students">Students  </Link></li>
         </ul>
     </div>
   )
