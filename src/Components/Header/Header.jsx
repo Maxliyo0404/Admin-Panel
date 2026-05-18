31ac368ef9a2539620f57dd666067c30ca1cd53d
Mahliyo <gasandigital13@gmail.com>
2026-05-18 19:15:17 +0500

    Layout

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,7 +5,11 @@ import React from 'react'
 function Header() {
   return (
     <header className="header">
-   
+   <div className="header-container">
+    <form className="form">
+      <input type="text" required placeholder="Name is your ...." />
+    </form>
+   </div>
     </header>
   )
 }
