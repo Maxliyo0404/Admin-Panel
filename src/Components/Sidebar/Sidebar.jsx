7b17954fb9ae8b6124c1ffe253362d5658fc1412
Mahliyo <gasandigital13@gmail.com>
2026-05-18 06:16:21 +0500

    Admin Panel struktura

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -6,9 +6,9 @@ function Sidebar() {
     <div className="container">
         <h1 className="sidebar-title">Logo</h1>
         <ul className="sidebar-list">
-            <li><Link className="sidebat-link" to="contact"></Link></li>
-            <li><Link className="sidebat-link" to="contact"></Link></li>
-            <li><Link className="sidebat-link" to="contact"></Link></li>
+            <li><Link className="sidebat-link" to="contacts"></Link></li>
+            <li><Link className="sidebat-link" to="teachers"></Link></li>
+            <li><Link className="sidebat-link" to="students"></Link></li>
         </ul>
     </div>
   )
