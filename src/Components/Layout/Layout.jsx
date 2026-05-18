968fb64ed8ea88ae1851bea50737078d5e8b3acd
Mahliyo <gasandigital13@gmail.com>
2026-05-18 06:46:21 +0500

    Admin Panel struktura logo

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -2,12 +2,14 @@ import { Outlet } from "react-router-dom";
 import "./Layout.css";
 import React from 'react'
 import Header from "../Header/Header";
+import Sidebar from "../Sidebar/Sidebar";
 
 
 function Layout() {
   return (
     <div>
-        <Header/>
+        <Header/> 
+        <Sidebar/>
         <main>
             <Outlet/>
         </main>
