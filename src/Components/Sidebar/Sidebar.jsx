dbd66cd5aec321030d522ef5db63c94c2a7db1d0
Mahliyo <gasandigital13@gmail.com>
2026-05-18 06:15:09 +0500

    Admin Panel struktura

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
 import "./Sidebar.css";
 import React from 'react'
-
 function Sidebar() {
   return (
     <div className="container">
         <h1 className="sidebar-title">Logo</h1>
+        <ul className="sidebar-list">
+            <li><Link className="sidebat-link" to="contact"></Link></li>
+            <li><Link className="sidebat-link" to="contact"></Link></li>
+            <li><Link className="sidebat-link" to="contact"></Link></li>
+        </ul>
     </div>
   )
 }
