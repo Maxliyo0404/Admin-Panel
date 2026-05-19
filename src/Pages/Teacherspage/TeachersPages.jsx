a006157fe48a999c16012754ddc21fc64917917d
Mahliyo <gasandigital13@gmail.com>
2026-05-19 07:25:48 +0500

    teachers css

--- src/Pages/Teacherspage/TeachersPages.jsx
+++ src/Pages/Teacherspage/TeachersPages.jsx
@@ -2,11 +2,15 @@ import "./TeachersPages.css";
 import React, { useEffect, useState } from 'react'
 
 const TeachersPages = () =>{
-  const [teachers, setTeachers]= useState([];
+  const [teachers, setTeachers]= useState([]);
     useEffect(() =>{
-      fetch("")
+      fetch("https://6a0bc87e5aa893e1015a7b82.mockapi.io/Teachers")
+      .then(response => response.json())
+      .then(teacher =>{
+        setTeachers(teachers)
+      })
     })
-  )
+
   return (
     <div>TeachersPages</div>
   )
