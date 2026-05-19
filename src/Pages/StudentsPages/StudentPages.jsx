f849a2ae7a579401c275095443bc474fd226edfc
Mahliyo <gasandigital13@gmail.com>
2026-05-19 06:16:26 +0500

    Layout fetch

--- src/Pages/StudentsPages/StudentPages.jsx
+++ src/Pages/StudentsPages/StudentPages.jsx
@@ -3,7 +3,9 @@ import React, { useEffect, useState } from 'react'
 
 const StudentPages = () => {
   const [students, setStudents] = useState([]);
-  useEffect(()=>)
+  useEffect(()=>{
+    fetch("https://6a0bb62a5aa893e1015a6639.mockapi.io/Students")
+  })
   return (
     <div></div>
   )
