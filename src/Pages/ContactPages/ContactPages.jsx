0179a8bfcbdc35746159bd6db1852e0d695671c8
Mahliyo <gasandigital13@gmail.com>
2026-05-19 08:47:14 +0500

    contacts css

--- src/Pages/ContactPages/ContactPages.jsx
+++ src/Pages/ContactPages/ContactPages.jsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react'
 const ContactPages = () => {
   const [contact, setContact] = useState([]);
   useEffect(()=>{
-    fetch("")
+    fetch("https://6a0bc87e5aa893e1015a7b82.mockapi.io/contacts")
     .then(response => response.json())
     .then(contact =>{
       setContact(contact)
